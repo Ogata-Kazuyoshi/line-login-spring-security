b5f10388f4acb96620ed486be1db92bf6508f36c
Ogata-Kazuyoshi <kazuyoshi_ogata_ab@mail.toyota.co.jp>
2024-03-31 17:16:38 +0900

    各providerのsuccesshandlerテストを追加

---
+++ backend/src/test/kotlin/com/example/backend/auth/handler/provider/GithubAuthenticationSuccessHandlerTests.kt
@@ -0,0 +1,43 @@
+package com.example.backend.auth.handler.provider
+
+import com.example.backend.service.UserService
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.security.oauth2.core.user.OAuth2User
+
+class GithubAuthenticationSuccessHandlerTests {
+    private lateinit var githubAuth:GithubAuthenticationSuccessHandler
+    private lateinit var userService:UserService
+    private lateinit var principal:OAuth2User
+
+    @BeforeEach
+    fun setUp () {
+        userService = mockk()
+        principal = mockk()
+        githubAuth = GithubAuthenticationSuccessHandler(userService)
+    }
+
+    @Test
+    fun `getOidメソッドは正しい引数でgetAttributeを呼んでいて、正しい値を返す`(){
+        every { principal.getAttribute<String>(any()) } returns "hogehoge"
+
+        val res = githubAuth.getOid(principal)
+
+        verify { principal.getAttribute<String>("node_id") }
+        assertEquals("hogehoge",res)
+    }
+
+    @Test
+    fun `getDisplayNameメソッドは正しい引数でgetAttributeを呼んでいて、正しい値を返す`(){
+        every { principal.getAttribute<String>(any()) } returns "hogehoge"
+
+        val res = githubAuth.getDisplayName(principal)
+
+        verify { principal.getAttribute<String>("login") }
+        assertEquals("hogehoge",res)
+    }
+}
---
+++ backend/src/test/kotlin/com/example/backend/auth/handler/provider/GoogleAuthenticationSuccessHandlerTests.kt
@@ -0,0 +1,43 @@
+package com.example.backend.auth.handler.provider
+
+import com.example.backend.service.UserService
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.security.oauth2.core.user.OAuth2User
+
+class GoogleAuthenticationSuccessHandlerTests {
+    private lateinit var googleAuth:GoogleAuthenticationSuccessHandler
+    private lateinit var userService:UserService
+    private lateinit var principal:OAuth2User
+
+    @BeforeEach
+    fun setUp () {
+        userService = mockk()
+        principal = mockk()
+        googleAuth = GoogleAuthenticationSuccessHandler(userService)
+    }
+
+    @Test
+    fun `getOidメソッドは正しい引数でgetAttributeを呼んでいて、正しい値を返す`(){
+        every { principal.getAttribute<String>(any()) } returns "hogehoge"
+
+        val res = googleAuth.getOid(principal)
+
+        verify { principal.getAttribute<String>("sub") }
+        assertEquals("hogehoge",res)
+    }
+
+    @Test
+    fun `getDisplayNameメソッドは正しい引数でgetAttributeを呼んでいて、正しい値を返す`(){
+        every { principal.getAttribute<String>(any()) } returns "hogehoge"
+
+        val res = googleAuth.getDisplayName(principal)
+
+        verify { principal.getAttribute<String>("name") }
+        assertEquals("hogehoge",res)
+    }
+}
